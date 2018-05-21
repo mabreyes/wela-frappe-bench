684e37371595de1a12c5366236a652f11ba2b418
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-05-21 14:55:22 +0800

    Add implementation for built-in Frappe charts for script report

--- wela_analytics/wela_analytics/report/w_student_full_report/w_student_full_report.py
+++ wela_analytics/wela_analytics/report/w_student_full_report/w_student_full_report.py
@@ -3,13 +3,28 @@
 
 from __future__ import unicode_literals
 import frappe
+import ast
 
 
 def execute(filters=None):
     columns, data = show_student_grade_history(
         filters.get('w_student'), filters.get('w_school_year'),
         filters.get('w_subject_short'))
+
+    data_for_chart = data[:]
+
+    data_for_chart = remove_brackets(data_for_chart)
+
+    # chart = show_data(columns, data_for_chart.get('first_quarter_grade'),
+    #                   data_for_chart.get('second_quarter_grade'),
+    #                   data_for_chart.get('third_quarter_grade'),
+    #                   data_for_chart.get('fourth_quarter_grade'))
+    print(data_for_chart)
     return columns, data
+
+
+def remove_brackets(list):
+    return ast.literal_eval(str(list).replace('[', '').replace(']', ''))
 
 
 def show_student_grade_history(student, school_year, subject_short):
@@ -132,5 +147,46 @@ def show_student_grade_history(student, school_year, subject_short):
 																	  AND tabw_student_grade.subject_name LIKE '{1}%'".format(student, subject_short))
 
     result = frappe.db.sql(student_grade_history_sql, as_dict=True)
-
+    print(result)
     return columns, result
+
+
+def show_data(columns, first_quarter_grade, second_quarter_grade, third_quarter_grade, fourth_quarter_grade):
+    x_intervals = ['x'] + [d.get("label") for d in columns[3:]]
+    first_quarter_grade_data, second_quarter_grade_data, third_quarter_grade_data, fourth_quarter_grade_data = [], [], [], []
+
+    for p in columns[3:]:
+        if first_quarter_grade:
+            first_quarter_grade_data.append(
+                first_quarter_grade[0].get(p.get('first_quarter_grade')))
+        if second_quarter_grade:
+            second_quarter_grade_data.append(
+                second_quarter_grade[0].get(p.get('second_quarter_grade')))
+        if third_quarter_grade:
+            third_quarter_grade_data.append(
+                third_quarter_grade[0].get(p.get('third_quarter_grade')))
+        if fourth_quarter_grade:
+            fourth_quarter_grade_data.append(
+                fourth_quarter_grade[0].get(p.get('fourth_quarter_grade')))
+
+    columns = [x_intervals]
+
+    if first_quarter_grade_data:
+        columns.append(['First Quarter'] + first_quarter_grade_data)
+    if second_quarter_grade_data:
+        columns.append(['Second Quarter'] + second_quarter_grade_data)
+    if third_quarter_grade_data:
+        columns.append(['Third Quarter'] + third_quarter_grade_data)
+    if fourth_quarter_grade_data:
+        columns.append(['Fourth Quarter'] + fourth_quarter_grade_data)
+
+    chart = {
+        "data": {
+            'x': 'x',
+            'columns': columns
+        }
+    }
+
+    chart['chart_type'] = "bar"
+
+    return chart
